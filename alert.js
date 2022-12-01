ecffbd00d2d9b462c22176d36512c6ca7edbcd37
E7Beauty <feniksraw@gmail.com>
2022-12-01 14:24:36 +0300

    firstCommit

---
+++ alert.js
@@ -0,0 +1,9 @@
+let message = prompt('Какое "официальное" название JavaScript?','');
+let user;
+alert(user??'Аноним');
+alert(message);
+if (message = 'ECMAScript') {
+	alert('Верно!');
+} else {
+	alert('Не знаете? “ECMAScript”!');
+}
